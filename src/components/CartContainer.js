18be23195178ea255d45435a5acb55b6d8884d51
rudinkaca <rudin.kaca@crispybacon.al>
2023-05-19 16:34:33 +0200

    feat: added clearCart action to cart-slice

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,8 +1,10 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import CartItem from "./CartItem";
+import {clearCart} from "../feature/cart/cartSlice";
 
 const CartContainer = () => {
     const {cartItems, total, amount} = useSelector(state => state.cart);
+    const dispatch = useDispatch();
 
     if (amount < 1) {
         return (
@@ -30,7 +32,12 @@ const CartContainer = () => {
                 <div className="cart-total">
                     <h4>total <span>${total}</span></h4>
                 </div>
-                <button className="btn clear-btn">clear cart</button>
+                <button
+                    className="btn clear-btn"
+                    onClick={() => {
+                        dispatch(clearCart());
+                    }}
+                >clear cart</button>
             </footer>
         </section>
     )
