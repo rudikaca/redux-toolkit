397cea31b5ea6e581973266a043262db677e4215
rudinkaca <rudin.kaca@crispybacon.al>
2023-05-22 16:32:16 +0200

    feat: added other functions to cart-slice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {calculateTotals} from "./feature/cart/cartSlice";
 
 function App() {
-  return (
+    const dispatch = useDispatch();
+    const {cartItems} = useSelector(state => state.cart);
+    useEffect(() => {
+        dispatch(calculateTotals());
+    }, [cartItems, dispatch]);
+
+    return (
       <main>
         <Navbar />
           <CartContainer />
       </main>
-  );
+    );
 }
 export default App;
